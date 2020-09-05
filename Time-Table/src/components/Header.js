33d61306d2c02448ee7744e71fd1e2eac0fdc9fb
Yash Parmar <yashparmar15700@gmail.com>
2020-09-05 15:10:25 +0530

    google signin added

--- Time-Table/src/components/Header.js
+++ Time-Table/src/components/Header.js
@@ -2,20 +2,52 @@ import React, { Component } from 'react';
 import * as Font from 'expo-font';
 import { Container, Header, Left, Body, Right, Button, Icon, Title,Text } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import {Alert } from 'react-native';
+import firebase from 'firebase';
 export default class HeaderCompo extends Component {
     constructor(props){
         super(props);
     }
     state = {
-        loading : true
+        loading : true,
+        loggedin : false
     }
     async componentDidMount() {
+        if(firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                console.log(firebase.auth().currentUser.displayName);
+                this.setState({loggedin : true});
+            }
+        }))
         await Font.loadAsync({
             Roboto: require('native-base/Fonts/Roboto.ttf'),
             Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
           });
         this.setState({loading : false});
       }
+
+      logoutUser = () => {
+        Alert.alert(
+            "Alert Title",
+            "My Alert Msg",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") }
+            ],
+            { cancelable: false }
+        );
+      }
+
+      logout = () => {
+        this.setState({loggedin : false})
+        firebase.auth().signOut();
+      }
+
   render() {
     return (
     
@@ -28,12 +60,16 @@ export default class HeaderCompo extends Component {
               </Button>
             </Left>
             <Body>
-                  <Title>{this.props.title}</Title>
+          <Title>{this.props.title}</Title>
             </Body>
             <Right>
-                {this.props.show ? <Button transparent onPress = {() => this.props.navigation.navigate('Login')}>
+                {this.props.show ? !this.state.loggedin ? <Button transparent onPress = {() => this.props.navigation.navigate('Login')}>
               <Ionicons name="md-add" size={28} color="white" />
-              </Button> : null}
+              </Button> : 
+                <Button transparent onPress = {this.logoutUser() }>
+                <Entypo name="log-out" size={24} color="white" />
+                </Button>
+                  : null}
             </Right>
           </Header>
           }
--- Time-Table/src/screens/Login.js
+++ Time-Table/src/screens/Login.js
@@ -3,11 +3,94 @@ import {Button} from 'react-native';
 import { Container, Content, Form, Item, Input, Label , Toast,Root, Text } from 'native-base';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import * as Google from 'expo-google-app-auth';
+import firebase from 'firebase';
 export default class LoginScreen extends Component {
     state = {
         user : "",
         pass : ""
     }
+    isUserEqual = (googleUser, firebaseUser) => {
+        if (firebaseUser) {
+          var providerData = firebaseUser.providerData;
+          for (var i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === googleUser.getBasicProfile().getId()) {
+              // We don't need to reauth the Firebase connection.
+              return true;
+            }
+          }
+        }
+        return false;
+      }
+    onSignIn = (googleUser) => {
+        // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+        var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+          unsubscribe();
+          // Check if we are already signed-in Firebase with the correct user.
+          if (!this.isUserEqual(googleUser, firebaseUser)) {
+            // Build Firebase credential with the Google ID token.
+            var credential = firebase.auth.GoogleAuthProvider.credential(
+                googleUser.idToken,
+                googleUser.accessToken
+            )
+            // Sign in with credential from the Google user.
+            firebase.auth().signInWithCredential(credential).then(result => {
+                if(result.additionalUserInfo.isNewUser)
+                {
+                    firebase.database().ref('users/' + result.user.uid)
+                    .set({
+                        gmail : result.user.email,
+                        profile_picture : result.additionalUserInfo.profile.picture,
+                        locale : result.additionalUserInfo.profile.locale,
+                        first_name : result.additionalUserInfo.profile.given_name,
+                        last_name : result.additionalUserInfo.profile.family_name,
+                        created_at : Date.now()
+                    })
+                } else {
+                    firebase.database().ref('users/' + result.user.uid)
+                    .update({
+                        last_logged_in : Date.now()
+                    })
+                }
+                this.props.navigation.navigate('Home');
+                console.log("Logged in");
+            })
+            
+            .catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              // ...
+            });
+          } else {
+            console.log('User already signed-in Firebase.');
+          }
+        }.bind(this));
+      }
+     signInWithGoogleAsync = async () => {
+        try {
+          const result = await Google.logInAsync({
+              behavior : 'web',
+            androidClientId: '671031154846-q7gv044sasbsi5e1oqr2osm56nfbibbj.apps.googleusercontent.com',
+            // iosClientId: YOUR_CLIENT_ID_HERE,
+            scopes: ['profile', 'email'],
+          });
+      
+          if (result.type === 'success') {
+              this.onSignIn(result);
+            return result.accessToken;
+          } else {
+            return { cancelled: true };
+          }
+        } catch (e) {
+          return { error: true };
+        }
+      }
     componentWillUnmount() { if (Toast.toastInstance != null && Toast.toastInstance.root != null) { Toast.hide(); } }
     CheckPass = () => {
         if(this.state.user === ""){
@@ -47,7 +130,7 @@ export default class LoginScreen extends Component {
               <Label style = {{fontSize : 15 , fontFamily : 'monospace'}}>Password</Label>
               <Input secureTextEntry={true} style = {{marginBottom : 50 , borderColor : '#808080' , borderBottomWidth : 1}} value = {this.state.pass} onChangeText = {(text) => this.setState({pass : text})}/>
             </Item>
-            <Button style = {{marginTop : 20}} color = "black"  title = "Login"  onPress = {() => this.CheckPass()}></Button>
+            <Button style = {{marginTop : 20}} color = "black"  title = "Login"  onPress = {() => this.signInWithGoogleAsync()}></Button>
         </Content>
         <Footer navigation = {this.props.navigation}/>
       </Container>
